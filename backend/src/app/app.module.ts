d300dddb88ac97f44ee3d17b6056c306a51a7533
Felipe Mateus <ipemateusribeiro@gmail.com>
2024-11-30 23:37:28 -0300

    fix: arrumar a leitura de datas do dto

--- backend/src/app/app.module.ts
+++ backend/src/app/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DataController } from 'src/modules/dataBase/database.controller';
 import { DatabaseModule } from 'src/modules/dataBase/database.module';
+import { ConsultaModule } from 'src/modules/consulta/consulta.module';
 
 @Module({
-    imports: [DatabaseModule],
+    imports: [DatabaseModule, ConsultaModule],
     controllers: [AppController, DataController],
     providers: [AppService],
 })
--- backend/src/modules/Consulta/dto/create-consulta.dto.ts
+++ backend/src/modules/Consulta/dto/create-consulta.dto.ts
@@ -1,4 +1,5 @@
-import { IsNotEmpty, IsEmail, IsString, IsDate, IsEnum, Matches } from 'class-validator';
+import { IsNotEmpty, IsString, IsDate, IsEmail, Matches, IsEnum } from 'class-validator';
+import { Transform, Type } from 'class-transformer';
 import { BodyType } from 'src/modules/dataBase/entities/consulta.entity';
 
 export class CreateConsultaDto {
@@ -8,14 +9,26 @@ export class CreateConsultaDto {
 
     @IsNotEmpty()
     @IsDate()
+    @Transform(({ value }) => {
+        const [day, month, year] = value.split('/'); // separando os atributos da data
+        return new Date(`${year}-${month}-${day}`); // retorando a data no formato esperado pelo mongoose
+    })
     date: Date;
 
     @IsNotEmpty()
     @IsDate()
+    @Transform(({ value }) => {
+        const [day, month, year] = value.split('/');
+        return new Date(`${year}-${month}-${day}`);
+    })
     startTime: Date;
 
     @IsNotEmpty()
     @IsDate()
+    @Transform(({ value }) => {
+        const [day, month, year] = value.split('/');
+        return new Date(`${year}-${month}-${day}`);
+    })
     endTime: Date;
 
     @IsNotEmpty()
@@ -27,15 +40,21 @@ export class CreateConsultaDto {
     email: string;
 
     @IsNotEmpty()
-    @Matches(/^\(?\d{2}\)?\s?\d{4,5}-?\d{4}$/, { message: 'Invalid phone number format' })
+    @Matches(/^\(?\d{2}\)?\s?\d{4,5}-?\d{4}$/, { message: 'Phone number format is invalid' })
     phone: string;
 
     @IsNotEmpty()
     @IsDate()
+    @Transform(({ value }) => {
+        const [day, month, year] = value.split('/');
+        return new Date(`${year}-${month}-${day}`);
+    })
     Birthdate: Date;
 
     @IsNotEmpty()
-    @IsEnum(BodyType)
+    @IsEnum(BodyType, {
+        message: 'biotipoCorporal must be one of the following values: Ectomorfo, Mesomorfo, Endomorfo',
+    })
     biotipoCorporal: BodyType;
 
     @IsNotEmpty()
