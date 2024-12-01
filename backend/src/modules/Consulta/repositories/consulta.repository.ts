34c4127be26622bd833e456b02a6c3bddff75d33
Felipe Mateus <ipemateusribeiro@gmail.com>
2024-11-30 22:45:25 -0300

    feat: criar o opcao de criacao de consulta no repositorio de consutla

---
+++ backend/src/modules/Consulta/repositories/consulta.repository.ts
@@ -0,0 +1,19 @@
+import { ConsultaDocument, ConsultaEntity } from 'src/modules/dataBase/entities/consulta.entity';
+import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { CreateConsultaDto } from '../dto/create-consulta.dto';
+
+@Injectable()
+export class consultaRepository {
+    constructor(@InjectModel(ConsultaEntity.name) private readonly consultaModel: Model<ConsultaDocument>) {}
+
+    async createConsulta(createConsultaDto: CreateConsultaDto): Promise<ConsultaDocument> {
+        try {
+            const createdConsulta = new this.consultaModel(createConsultaDto);
+            return createdConsulta.save();
+        } catch (error) {
+            throw error;
+        }
+    }
+}
