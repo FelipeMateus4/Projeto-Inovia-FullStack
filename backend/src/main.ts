397d75f15ebb9715406edc187a40cb90caf23453
Felipe Mateus <ipemateusribeiro@gmail.com>
2024-12-02 13:42:04 -0300

    chore: adicionar um comentario decescritivo para o pipes do class validadtor

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -8,6 +8,7 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
 
+    // Configuração do uso de pipes para validação
     app.useGlobalPipes(
         new ValidationPipe({
             whitelist: true,
