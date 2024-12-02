e181aabf95e5ccb0315efeb4673df75d7fca090b
Felipe Mateus <ipemateusribeiro@gmail.com>
2024-12-01 21:31:49 -0300

    delete: removendo arquivos iniciais inuteis

--- backend/src/app/app.module.ts
+++ backend/src/app/app.module.ts
@@ -1,7 +1,4 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { DataController } from 'src/modules/dataBase/database.controller';
 import { DatabaseModule } from 'src/modules/dataBase/database.module';
 import { ConsultaModule } from 'src/modules/consulta/consulta.module';
 import { APP_FILTER } from '@nestjs/core';
@@ -9,9 +6,8 @@ import { AllExceptionsFilter } from 'src/shared/exceptions.filter';
 
 @Module({
     imports: [DatabaseModule, ConsultaModule],
-    controllers: [AppController, DataController],
+    controllers: [],
     providers: [
-        AppService,
         {
             provide: APP_FILTER,
             useClass: AllExceptionsFilter, // Registro global do filtro
