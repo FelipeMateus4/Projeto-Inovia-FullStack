7ebc3396461a3621142b0fee9eb979b330b273a7
Felipe Mateus <ipemateusribeiro@gmail.com>
2024-12-07 02:31:54 -0300

    feat: adicionar separacao ao modulo de documentacao do sswagger

--- backend/src/modules/auth/controller/auth.controller.ts
+++ backend/src/modules/auth/controller/auth.controller.ts
@@ -2,11 +2,13 @@ import { Controller, Post, Req, Res, UseGuards } from '@nestjs/common';
 import { Request, Response } from 'express';
 import { AuthService } from '../services/auth.service';
 import { LocalAuthGuard } from '../guards/local-auth.guard';
+import { ApiTags } from '@nestjs/swagger';
 
 @Controller('auth')
 export class AuthController {
     constructor(private readonly authService: AuthService) {}
 
+    @ApiTags('Auth')
     @UseGuards(LocalAuthGuard)
     @Post('login')
     async login(@Req() req: Request, @Res() res: Response) {
