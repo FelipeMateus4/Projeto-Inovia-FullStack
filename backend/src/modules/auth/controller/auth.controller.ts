7fc93a610248dc90b0f0fbaf0d229577dabb769e
Felipe Mateus <ipemateusribeiro@gmail.com>
2024-12-12 22:24:10 -0300

    feat: adicionar mais documentacao ao swagger

--- backend/src/modules/auth/controller/auth.controller.ts
+++ backend/src/modules/auth/controller/auth.controller.ts
@@ -2,13 +2,24 @@ import { Controller, Post, Req, Res, UseGuards } from '@nestjs/common';
 import { Request, Response } from 'express';
 import { AuthService } from '../services/auth.service';
 import { LocalAuthGuard } from '../guards/local-auth.guard';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiTags, ApiOperation, ApiResponse, ApiBody } from '@nestjs/swagger';
+import { ApiProperty } from '@nestjs/swagger';
 
+export class LoginDto {
+    @ApiProperty({ description: 'Nome de usuário', example: 'user123' })
+    username: string;
+
+    @ApiProperty({ description: 'Senha do usuário', example: 'password123' })
+    password: string;
+}
 @Controller('auth')
 export class AuthController {
     constructor(private readonly authService: AuthService) {}
 
     @ApiTags('Auth')
+    @ApiOperation({ summary: 'Realiza login na aplicação' })
+    @ApiResponse({ status: 200, description: 'Login realizado com sucesso.' })
+    @ApiBody({ description: 'Credenciais de login', type: LoginDto })
     @UseGuards(LocalAuthGuard)
     @Post('login')
     async login(@Req() req: Request, @Res() res: Response) {
@@ -20,6 +31,8 @@ export class AuthController {
         }).send({ message: 'Login successful' });
     }
 
+    @ApiOperation({ summary: 'Realiza logout na aplicação' })
+    @ApiResponse({ status: 200, description: 'Logout realizado com sucesso.' })
     @Post('logout')
     async logout(@Res() res: Response) {
         res.clearCookie('jwt').send({ message: 'Logout successful' });
--- backend/src/modules/users/controller/user.controller.ts
+++ backend/src/modules/users/controller/user.controller.ts
@@ -2,11 +2,16 @@ import { Controller, Get, UseGuards } from '@nestjs/common';
 import { JwtAuthGuard } from 'src/modules/auth/guards/jwt-auth.guard';
 import { Model } from 'mongoose';
 import { InjectModel } from '@nestjs/mongoose';
+import { ApiTags, ApiOperation, ApiResponse } from '@nestjs/swagger';
 
 @Controller('users')
 export class UserController {
     constructor(@InjectModel('auths') private readonly userModel: Model<any>) {}
 
+    @ApiTags('Users')
+    @ApiOperation({ summary: 'Busca todos os usuários' })
+    @ApiResponse({ status: 200, description: 'Retorna os dados dos usuarios' })
+    @ApiResponse({ status: 500, description: 'Erro interno no servidor.' })
     @UseGuards(JwtAuthGuard)
     @Get()
     async findAll(): Promise<any> {
